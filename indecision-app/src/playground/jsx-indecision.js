fc5af031f66dfaacf653bdfba601990c5d518a65
shocklaing <shockwater@gmail.com>
2019-03-06 20:57:13 +0800

    Backup first jsx app.

---
+++ indecision-app/src/playground/jsx-indecision.js
@@ -0,0 +1,60 @@
+const app = {
+    title: "Indecision App",
+    subTitle: "First react app",
+    // options: ["One", "Two"]
+    options: []
+  };
+  
+  const onFormSubmit = e => {
+    e.preventDefault();
+  
+    const option = e.target.elements.option.value;
+    if (option) {
+      app.options.push(option);
+      e.target.elements.option.value = "";
+      renderTemplate();
+    }
+  };
+  
+  const onRemoveAll = () => {
+    app.options = [];
+    renderTemplate();
+  };
+  
+  const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+  };
+  
+  // JSX
+  const renderTemplate = () => {
+    const template = (
+      <div>
+        <h1>{app.title}</h1>
+        {app.subTitle && <p>{app.subTitle}</p>}
+        {app.options && app.options.length > 0
+          ? "Here are your options"
+          : "No options"}
+  
+        <button disabled={app.options.length == 0} onClick={onMakeDecision}>
+          What should I do?
+        </button>
+        <button onClick={onRemoveAll}>Remove All</button>
+        <ol>
+          {app.options.map(option => (
+            <li key={option}>{option}</li>
+          ))}
+        </ol>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(template, appRoot);
+  };
+  
+  const appRoot = document.getElementById("app");
+  renderTemplate();
+  
