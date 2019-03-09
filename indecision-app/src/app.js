8bb1a96f1ca03ebc4e15c953bf33f6e251ebedcb
shocklaing <shockwater@gmail.com>
2019-03-09 21:26:17 +0800

    Method binding.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -40,8 +40,14 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+
   handleRemoveAll() {
-    alert("handleRemoveAll");
+    // alert("handleRemoveAll");
+    console.log(this.props.options);
   }
 
   render() {
