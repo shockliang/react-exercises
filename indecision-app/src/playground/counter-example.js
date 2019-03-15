6a1c885aae2ec9e1d9a57e576c0c03ffe5a5f08a
shocklaing <shockwater@gmail.com>
2019-03-16 00:54:23 +0800

    Saving and loading the counter.

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,8 +5,21 @@ class Counter extends React.Component {
     this.handleMinueOne = this.handleMinueOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
+  }
+
+  componentDidMount() {
+    const count = parseInt(localStorage.getItem("count"), 10);
+    if (!isNaN(count)) {
+      this.setState(() => ({ count }));
+    }
+  }
+
+  componentDidUpdate(prveProps, prveState) {
+    if (prveState.count !== this.state.count) {
+      localStorage.setItem("count", this.state.count);
+    }
   }
 
   handleAddOne() {
@@ -45,8 +58,8 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 0
-};
+// Counter.defaultProps = {
+//   count: 0
+// };
 
 ReactDOM.render(<Counter />, document.getElementById("app"));
