142a855f165f2a5dc1bf2397d82a9dcf1aa7d3ba
shocklaing <shockwater@gmail.com>
2019-03-18 21:43:56 +0800

    Importing npm module.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,15 +1,6 @@
-// import "./utils";
-// import subtract, { square, add } from "./utils";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is running!");
-// console.log(square(4));
-// console.log(add(1, 3));
-// console.log(subtract(100, 19));
+const template = React.createElement("p", {}, "testing");
 
-import isSenior, { isAdult, canDrink } from "./person";
-
-console.log(
-  `20 years old is adult: ${isAdult(20)}, and can drink:${canDrink(20)}`
-);
-
-console.log(`66 years old is senior: ${isSenior(66)}`);
+ReactDOM.render(template, document.getElementById("app"));
