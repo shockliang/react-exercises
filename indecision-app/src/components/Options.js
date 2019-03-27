8a73cccba18f8af85dba8027eb43c1a3fa37935f
shocklaing <shockwater@gmail.com>
2019-03-28 00:29:30 +0800

    Styling option item.

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -12,11 +12,14 @@ const Options = props => (
         Remove All
       </button>
     </div>
-    {props.options.length === 0 && <p className="widget__message">Please add an option to get started</p>}
-    {props.options.map(option => (
+    {props.options.length === 0 && (
+      <p className="widget__message">Please add an option to get started</p>
+    )}
+    {props.options.map((option, index) => (
       <Option
         key={option}
         optionText={option}
+        count={index + 1}
         handleDeleteOption={props.handleDeleteOption}
       />
     ))}
