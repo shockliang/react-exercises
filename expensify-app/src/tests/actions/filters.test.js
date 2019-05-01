47feb6a952e9ada25e6bcffe28bb8d0e0595510e
shockliang <shockwater@gmail.com>
2019-05-02 00:26:35 +0800

    Testing filter action generators.

---
+++ expensify-app/src/tests/actions/filters.test.js
@@ -0,0 +1,49 @@
+import {
+  setTextFilter,
+  sortByAmount,
+  sortByDate,
+  setStartDate,
+  setEndDate
+} from "../../actions/filters";
+import moment from "moment";
+
+test("should generate set start date filter object", () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: "SET_START_DATE",
+    startDate: moment(0)
+  });
+});
+
+test("should generate set end date filter object", () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: "SET_END_DATE",
+    endDate: moment(0)
+  });
+});
+
+test("should generate set text filter object wtih text value", () => {
+  const text = "rent";
+  const action = setTextFilter(text);
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text
+  });
+});
+
+test("should generate set text filter object witn default", () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
+  });
+});
+
+test("should generate sort by amount filter object", () => {
+  expect(sortByAmount()).toEqual({ type: "SORT_BY_AMOUNT" });
+});
+
+test("should generate sort by date filter object", () => {
+  expect(sortByDate()).toEqual({ type: "SORT_BY_DATE" });
+});
