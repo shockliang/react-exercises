b12a4bda5cdc7451e24870f7f048c6e6483b9e8c
shockliang <shockwater@gmail.com>
2019-05-17 21:03:35 +0800

    Creating separate css file.

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = env => {
   const isProduction = env === "production";
@@ -17,12 +18,44 @@ module.exports = env => {
           exclude: /node_modules/
         },
         {
-          test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === "development"
+              }
+            },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
         }
       ]
     },
-    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        // filename: !isProduction ? "[name].css" : "[name].[hash].css",
+        // chunkFilename: !isProduction ? "[id].css" : "[id].[hash].css"
+        // filename: "[name].css",
+        // chunkFilename: "[id].css"
+        filename: "styles.css",
+        chunkFilename: "[name].css"
+      })
+    ],
+    // devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true
