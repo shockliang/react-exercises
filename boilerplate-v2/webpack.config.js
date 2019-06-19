5c5763e3ce4c3740aa2290f63fd4ff195033fa8d
shockliang <shockwater@gmail.com>
2019-06-20 00:01:24 +0800

    Upgrading react boilerplate.

---
+++ boilerplate-v2/webpack.config.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (process.env.NODE_ENV === "test") {
+  require("dotenv").config({ path: ".env.test" });
+} else if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
+
+module.exports = env => {
+  const isProduction = env === "production";
+
+  return {
+    entry: ["babel-polyfill", "./src/app.js"],
+    output: {
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === "development"
+              }
+            },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        // filename: !isProduction ? "[name].css" : "[name].[hash].css",
+        // chunkFilename: !isProduction ? "[id].css" : "[id].[hash].css"
+        // filename: "[name].css",
+        // chunkFilename: "[id].css"
+        filename: "styles.css",
+        chunkFilename: "[name].css"
+      }),
+      new webpack.DefinePlugin({
+        "process.env.FIREBASE_API_KEY": JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        "process.env.FIREBASE_AUT_DOMAIN": JSON.stringify(
+          process.env.FIREBASE_AUT_DOMAIN
+        ),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+        "process.env.FIREBASE_APP_ID": JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        )
+      })
+    ],
+    // devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/"
+    },
+    mode: "development"
+  };
+};
