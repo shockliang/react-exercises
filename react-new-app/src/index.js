016cf3f6887c3f34c0c1344b6e1c492ac37033cb
shockliang <shockwater@gmail.com>
2019-06-23 00:25:51 +0800

    useState vs setState.

--- react-new-app/src/index.js
+++ react-new-app/src/index.js
@@ -2,15 +2,49 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+// bad way
+// const App = props => {
+//   const [state, setState] = useState({
+//     count: props.count,
+//     text: ""
+//   });
+
+//   return (
+//     <div>
+//       <p>
+//         The current {state.text || "count"} is {state.count}
+//       </p>
+//       <button onClick={() => setState({ ...state, count: state.count + 1 })}>
+//         +1
+//       </button>
+//       <button onClick={() => setState({ ...state, count: state.count - 1 })}>
+//         -1
+//       </button>
+//       <button onClick={() => setState({ ...state, count: props.count })}>
+//         reset
+//       </button>
+//       <input
+//         value={state.text}
+//         onChange={e => setState({ ...state, text: e.target.value })}
+//       />
+//     </div>
+//   );
+// };
+
+// Good way
 const App = props => {
   const [count, setCount] = useState(props.count);
+  const [text, setText] = useState("");
 
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>
+        The current {text || "count"} is {count}
+      </p>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(props.count)}>reset</button>
+      <input value={text} onChange={e => setText(e.target.value)} />
     </div>
   );
 };
