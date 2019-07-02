862f2e4d2b7a0c1aa0ab46a05f94ba170f9794e7
shockliang <shockwater@gmail.com>
2019-07-03 00:02:41 +0800

    Native component approach.

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00PM</span>
+          </div>
+          <div className="text">Still a good boy!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
