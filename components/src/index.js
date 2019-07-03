0b6ae49ebcc8f05b2f24a4f0260f57ab61908c1b
shockliang <shockwater@gmail.com>
2019-07-03 20:56:30 +0800

    Duplicating a single component.

--- components/src/index.js
+++ components/src/index.js
@@ -19,6 +19,34 @@ const App = () => {
           <div className="text">Still a good boy!</div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00PM</span>
+          </div>
+          <div className="text">Still a good boy!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00PM</span>
+          </div>
+          <div className="text">Still a good boy!</div>
+        </div>
+      </div>
     </div>
   );
 };
