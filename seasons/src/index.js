9df6da28d6acebb562603b0f46addcbc5e27e9e4
shockliang <shockwater@gmail.com>
2019-07-06 15:31:02 +0800

    Scaffolding the seasons app.

---
+++ seasons/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+const App = () => {
+  return (
+    <div>
+      <SeasonDisplay />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
