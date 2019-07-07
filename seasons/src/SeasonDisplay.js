2abdb700a892dbf8f71356109a11d9c1a78c2358
shockliang <shockwater@gmail.com>
2019-07-08 00:13:35 +0800

    Determining season.

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+// Northern Hemisphere
+// W    W   W   S   S   S   S   S   S     W   W   W
+// 0    1   2   3   4   5   6   7   8     9   10  11
+// Jan  Feb Mar Apr May Jun Jul Aug Sept  Oct Nov Dec
+
+// Southern Hemisphere
+// S    S   S   W   W   W   W   W   W     S   S   S
+// 0    1   2   3   4   5   6   7   8     9   10  11
+// Jan  Feb Mar Apr May Jun Jul Aug Sept  Oct Nov Dec
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summery";
+  }
+};
+
 const SeasonDisplay = ({ lat }) => {
-  return <div>Lat: {lat}</div>;
+  const season = getSeason(lat, new Date().getMonth());
+  return <div>Season: {season}</div>;
 };
 
 export { SeasonDisplay as default };
