6ff57219ac355a90fae26b39d265db327c2eaedc
shockliang <shockwater@gmail.com>
2019-07-10 09:05:52 +0800

    Adding some styling.

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 // Northern Hemisphere
@@ -33,10 +34,10 @@ const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth());
   const { text, iconName } = seasonConifg[season];
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
