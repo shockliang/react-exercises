57604617a0c87e3875f6b87d6c3bc8919197f819
shockliang <shockwater@gmail.com>
2019-07-12 23:17:10 +0800

    Handling form submital.

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,14 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
