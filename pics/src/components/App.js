5e2d0ed70a67f89db48bf9f53a973115c45f2d59
shockliang <shockwater@gmail.com>
2019-07-13 22:37:11 +0800

    Using axio to fetch data.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+      }
+    });
   }
 
   render() {
+    
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
