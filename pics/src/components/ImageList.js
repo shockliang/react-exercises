9ad44e456820ae6463701ec3c8f439b9c2f72bb6
shockliang <shockwater@gmail.com>
2019-07-15 00:01:30 +0800

    Building list of search results.

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ImageList = ({ images }) => {
+  const imageUrls = images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular} />;
+  });
+  return <div>{imageUrls}</div>;
+};
+
+export { ImageList as default };
