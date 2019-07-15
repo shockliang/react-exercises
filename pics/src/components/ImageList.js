6294bd43852d1b8e8540000c215a5e4304614802
shockliang <shockwater@gmail.com>
2019-07-15 21:45:21 +0800

    Applying grid css to image list.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = ({ images }) => {
   const imageUrls = images.map(({ description, id, urls }) => {
     return <img alt={description} key={id} src={urls.regular} />;
   });
-  return <div>{imageUrls}</div>;
+  return <div className="image-list">{imageUrls}</div>;
 };
 
 export { ImageList as default };
