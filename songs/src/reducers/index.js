4e6d594c49907d8e20c9d9bb663aa98980719319
shockliang <shockwater@gmail.com>
2019-07-25 22:26:26 +0800

    Building reducers.

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,18 @@
+const songsReducer = () => {
+  return [
+    { title: "Some song 1", duration: "4:15" },
+    { title: "Some song 2", duration: "2:15" },
+    { title: "Some song 3", duration: "3:15" },
+    { title: "Some song 4", duration: "4:15" },
+    { title: "Some song 5", duration: "5:15" },
+    { title: "Some song 6", duration: "1:15" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
