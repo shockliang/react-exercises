02c2938d774c395a9c9e6495f39149dddecfa6a1
shockliang <shockwater@gmail.com>
2019-09-13 21:24:14 +0800

    Starting relpace redux with context.

---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class LanguageSelector extends React.Component {
+  render() {
+    return (
+      <div>
+        Select a language:
+        <i
+          className="flag us"
+          onClick={() => this.props.onLanguageChange("english")}
+        />
+        <i
+          className="flag tw"
+          onClick={() => this.props.onLanguageChange("traditionChinese")}
+        />
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
