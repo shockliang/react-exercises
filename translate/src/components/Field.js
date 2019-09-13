9dbe84397ad5a974d99c9b2543a5f93b9a8cfcf7
shockliang <shockwater@gmail.com>
2019-09-14 00:03:08 +0800

    Connecting the field and button to the store.

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === "english" ? "Name" : "姓名";
+    const text = this.context.language === "english" ? "Name" : "姓名";
     return (
       <div className="ui field">
         <label>{text}</label>
--- translate/src/components/LanguageSelector.js
+++ translate/src/components/LanguageSelector.js
@@ -1,17 +1,20 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
     return (
       <div>
         Select a language:
         <i
           className="flag us"
-          onClick={() => this.props.onLanguageChange("english")}
+          onClick={() => this.context.onLanguageChange("english")}
         />
         <i
           className="flag tw"
-          onClick={() => this.props.onLanguageChange("traditionChinese")}
+          onClick={() => this.context.onLanguageChange("traditionChinese")}
         />
       </div>
     );
