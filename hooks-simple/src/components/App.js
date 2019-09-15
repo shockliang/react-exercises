043988851da740f2db53a3eebf8b175ae1e49285
shockliang <shockwater@gmail.com>
2019-09-15 14:42:38 +0800

    Initial hooks simple project.

---
+++ hooks-simple/src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class App extends React.Component {
+  state = { resource: "posts" };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          <button onClick={() => this.setState({ resource: "posts" })}>
+            Posts
+          </button>
+          <button onClick={() => this.setState({ resource: "todos" })}>
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
+  }
+}
+
+export default App;
