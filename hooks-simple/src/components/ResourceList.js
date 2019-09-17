d0b6fa413c49ab38db960ae9ae434f15ac2a76dd
shockliang <shockwater@gmail.com>
2019-09-18 00:20:31 +0800

    Fetching a given resource.

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,8 +1,19 @@
 import React from "react";
+import axios from "axios";
 
 class ResourceList extends React.Component {
+  state = { resource: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+
+    this.setState({ resource: response.data });
+  }
+
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resource.length}</div>;
   }
 }
 
