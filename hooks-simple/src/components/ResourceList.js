60fdf1c0e5628434203314ba914d3b2d95a0833e
shockliang <shockwater@gmail.com>
2019-09-19 00:25:04 +0800

    Using componentDidUpdate to update resource.

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -12,6 +12,16 @@ class ResourceList extends React.Component {
     this.setState({ resource: response.data });
   }
 
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+
+      this.setState({ resource: response.data });
+    }
+  }
+
   render() {
     return <div>{this.state.resource.length}</div>;
   }
